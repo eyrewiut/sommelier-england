89332927e98ef8e17ee84056e557bae1359af80f
George <gnewton155@gmail.com>
2019-11-30 12:48:28 +0000

    Clean up some issues with margin in error page Made posts display a default image on news page

--- routes/views/events.js
+++ routes/views/events.js
@@ -6,25 +6,28 @@ exports = module.exports = function(req, res) {
 	var view = new keystone.View(req, res);
 	var locals = res.locals;
 
+	//Add page properties to locals
 	locals.section = "events";
 	locals.pageTitle = "Events";
 	
+	//Called whenever the page is requested
 	view.on("init", function(next) {
+		//Find events from the database
 		Event.model.find().sort("-date").exec(function(err, results) {
 			if (err) {
 				return next(err);
 			}
-			
+			//Group events by year and add the year to the 
 			var e = _.groupBy(results, "year");
 			for (const key in e) {
-				if (e.hasOwnProperty(key)) {
+				if (Object.prototype.hasOwnProperty.call(e, key)) {
 					e[key].year = key;
 				}
 			}
+			//Order the events by year
 			locals.events = _.orderBy(e, "year", "desc");
-
 			next(err);
 		});
 	});
-	view.render("events");
+	view.render("events"); //Render the template into html
 };
